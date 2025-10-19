e6211476dd1470c264e540eccfedfab0c3407c72
Karen Behlarian <Gladiatorkaren@gmail.com>
2025-10-19 21:39:05 +0300

    add task solution

---
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -0,0 +1,111 @@
+import { FormEvent, useState } from 'react';
+import { Nullable } from '../../domain/Nullable';
+import users from '../../api/users';
+import todos from '../../api/todos';
+import { Todo } from '../../domain/Todo';
+
+type AddTodoFormProps = {
+  onSubmit: (todo: Todo) => void;
+};
+
+export const AddTodoForm = ({ onSubmit }: AddTodoFormProps) => {
+  const [title, setTitle] = useState('');
+  const [titleError, setTitleError] = useState<Nullable<string>>(null);
+
+  const [ownerId, setOwnerId] = useState<number>(0);
+  const [ownerErrorId, setOwnerErrorId] = useState<Nullable<string>>(null);
+
+  const handleResetForm = () => {
+    setTitle('');
+    setTitleError(null);
+    setOwnerId(0);
+    setOwnerErrorId(null);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (titleError) {
+      setTitleError(null);
+    }
+    setTitle(event.target.value.trimStart());
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setTitleError(null);
+    setOwnerErrorId(null);
+
+    const normalizedTitle = title.trim();
+    let hasError = false;
+
+    if (!normalizedTitle) {
+      setTitleError('Please enter a title');
+      hasError = true;
+    }
+
+    if (ownerId === 0) {
+      setOwnerErrorId('Please choose a user');
+      hasError = true;
+    }
+
+    if (hasError) {
+      return;
+    }
+
+    const maxId = Math.max(...todos.map(todo => todo.id), 0);
+
+    const newTodo: Todo = {
+      id: maxId + 1,
+      title: normalizedTitle,
+      completed: false,
+      userId: ownerId,
+    };
+
+    onSubmit(newTodo);
+    handleResetForm();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="field">
+        <input
+          type="text"
+          data-cy="titleInput"
+          placeholder="Enter a title"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        {titleError && <span className="error">{titleError}</span>}
+      </div>
+
+      <div className="field">
+        <select
+          data-cy="userSelect"
+          value={ownerId}
+          onChange={event => {
+            const newOwnerId = +event.target.value;
+
+            if (ownerErrorId && newOwnerId !== 0) {
+              setOwnerErrorId(null);
+            }
+
+            setOwnerId(newOwnerId);
+          }}
+        >
+          <option value="0">Choose a user</option>
+          {users.map(user => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+
+        {ownerErrorId && <span className="error">{ownerErrorId}</span>}
+      </div>
+
+      <button type="submit" data-cy="submitButton">
+        Add
+      </button>
+    </form>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import { TodoAggregate } from "../../domain/TodoAgregate";
+import { TodoInfo } from "../TodoInfo";
+
+type TodoListProps = {
+  todos: TodoAggregate[]
+}
+
+export const TodoList = ({todos} : TodoListProps) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo}/>
+      ))}
+    </section>
+  );
+};
+
